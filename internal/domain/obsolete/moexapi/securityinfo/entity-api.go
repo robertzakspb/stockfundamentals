600363bf32fcbe6a4efd9a631e53e4db07f6504f
robertzakspb <zakiev@me.com>
2025-08-06 16:00:49 +0200

    Removing the obsolete Moex API package

--- internal/domain/obsolete/moexapi/securityinfo/entity-api.go
+++
@@ -1,9 +0,0 @@
-//DISCLAIMER: THIS PACKAGE IS NO LONGER IN USE, IT MAY EVENTUALLY BE DELETED
-package securityinfo
-
-type MoexSecuritiesDTO struct {
-	Securities struct {
-		Columns []string        `json:"columns"`
-		Data    [][]interface{} `json:"data"`
-	} `json:"securities"`
-}
