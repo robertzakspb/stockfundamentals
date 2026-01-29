a40d5765b5a4d31ad50f4b71049afeea546ba734
Robert Zakiev <zakievrobert78@gmail.com>
2026-01-29 13:25:05 +0100

    minor changes in the logger

--- internal/infrastructure/config/config.go
+++ internal/infrastructure/config/config.go
@@ -4,6 +4,7 @@ package config
 import (
 	"os"
 	"github.com/joho/godotenv"
+	"github.com/compoundinvest/stockfundamentals/internal/infrastructure/logger"
 )
 
 type Config struct {
@@ -17,8 +18,8 @@ type YDBConfig struct {
 func LoadConfig() (*Config, error) {
 	err := godotenv.Load("dev.env")
 	if err != nil {
+		logger.Log("FAILED LOAD THE ENVIRONMENT FILE " + "dev.env", logger.ALERT)
 		return nil, err
-
 	}
 
 	dbConnectionString, ok := os.LookupEnv("DB_CONNECTION_STRING")
