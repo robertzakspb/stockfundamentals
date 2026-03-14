510fb3626b2575e988a2c865cff33ec92c92d063
robertzakspb <zakiev@me.com>
2026-03-14 23:57:53 +0100

    Slight refactoring

--- internal/interface/api/fundamentals/dividend/get.go
+++ internal/interface/api/fundamentals/dividend/get.go
@@ -9,23 +9,24 @@ import (
 
 func GetDividendForecasts(c *gin.Context) {
 	forecasts, err := appdividend.GetDividendForecasts()
+
 	if err != nil {
 		c.JSON(http.StatusInternalServerError, err.Error())
 		return
+	} else {
+		dtos := mapDividendForecastDomainToDto(forecasts)
+		c.JSON(http.StatusOK, dtos)
 	}
-
-	dtos := mapDividendForecastDomainToDto(forecasts)
-
-	c.JSON(http.StatusOK, dtos)
 }
 
 func GetDividendForecastsGroupedBySecurity(c *gin.Context) {
 	forecasts, err := appdividend.GetDivForecastsGroupedBySecurity()
+
 	if err != nil {
 		c.JSON(http.StatusInternalServerError, err.Error())
 		return
+	} else {
+		dtos := mapSecurityDivForecastToDto(forecasts)
+		c.JSON(http.StatusOK, dtos)
 	}
-
-	dtos := mapSecurityDivForecastToDto(forecasts)
-	c.JSON(http.StatusOK, dtos)
 }
