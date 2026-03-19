35d46e887dca352031e56e80447084b9379b5a4a
robertzakspb <zakiev@me.com>
2026-03-19 15:01:07 +0100

    Added the AssertNotEqual function

--- internal/test/helper.go
+++ internal/test/helper.go
@@ -2,18 +2,24 @@ package test
 
 import "testing"
 
-//TODO: Deprecate any replace with the generic method below AssertEqual[T comparable]()
-func AssertEqualStrings(t testing.TB, expected, actual  string) {
+// TODO: Deprecate any replace with the generic method below AssertEqual[T comparable]()
+func AssertEqualStrings(t testing.TB, expected, actual string) {
 	t.Helper()
 	if expected != actual {
 		t.Errorf("expected: %q; got: %q", expected, actual)
 	}
 }
 
-
-func AssertEqual[T comparable](t testing.TB, expected, actual  T) {
+func AssertEqual[T comparable](t testing.TB, expected, actual T) {
 	t.Helper()
 	if expected != actual {
 		t.Errorf("expected: %v; got: %v", expected, actual)
+	}
+}
+
+func AssertNotEqual[T comparable](t testing.TB, expected, actual T) {
+	t.Helper()
+	if expected == actual {
+		t.Errorf("Values should not be equal. Expected: %v; got: %v", expected, actual)
 	}
 }
