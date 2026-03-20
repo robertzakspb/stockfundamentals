b87c85511d6ca06d38153395ab00832c2ad983da
robertzakspb <zakiev@me.com>
2026-03-20 14:56:34 +0100

    started working on the ytm calculation functionality for the existing position lots

--- internal/domain/entities/bonds/bonds.go
+++ internal/domain/entities/bonds/bonds.go
@@ -5,7 +5,7 @@ import (
 	"strconv"
 	"time"
 
-	"github.com/compoundinvest/stockfundamentals/internal/domain/entities/compoundinterest"
+	"github.com/compoundinvest/stockfundamentals/internal/domain/entities/bonds"
 	"github.com/compoundinvest/stockfundamentals/internal/domain/entities/forex"
 	"github.com/google/uuid"
 )
@@ -41,6 +41,9 @@ type Bond struct {
 	RiskLevel               RiskLevel
 	BondType                BondType
 	CallOptionExerciseDate  time.Time
+	Bond                    bonds.Bond
+	Coupons                 []bonds.Coupon
+	YieldToMaturity         float64
 }
 
 type RiskLevel int
@@ -147,55 +150,4 @@ func (b Bond) validate() error {
 	}
 
 	return nil
-}
-
-func (b Bond) YieldToMaturity(coupons []Coupon, marketPrice float64) (float64, error) {
-	yield, err := calculateYield(b, coupons, marketPrice, time.Now(), b.MaturityDate)
-	if err != nil {
-		return -1, err
-	}
-
-	return yield, nil
-}
-
-func (b Bond) YieldToCallOption(coupons []Coupon, marketPrice float64) (float64, error) {
-	if b.CallOptionExerciseDate.IsZero() {
-		return -1, errors.New("Attempting to calculate a yield to call option for a bond without a call exercise date")
-	}
-
-	yield, err := calculateYield(b, coupons, marketPrice, time.Now(), b.MaturityDate)
-	if err != nil {
-		return -1, err
-	}
-
-	return yield, nil
-}
-
-// Calculates the return realized on the bond given a market price, including coupons and redemption
-// Coupon reinvestment is not assumed
-func totalBondReturn(bond Bond, coupons []Coupon, marketPrice float64, acquisitionDate, redemptionDate time.Time) (float64, error) {
-	if marketPrice == 0 {
-		return -1, errors.New("Invalid market price")
-	}
-
-	futureCashflows := totalCouponIncome(coupons, false) + bond.NominalValue
-
-	cumulativeReturn := futureCashflows / marketPrice - 1
-	totalReturn := compoundinterest.CalcAnnualizedReturn(cumulativeReturn, acquisitionDate, redemptionDate)
-
-	return totalReturn, nil
-}
-
-func calculateYield(b Bond, coupons []Coupon, marketPrice float64, acquisitionDate, redemptionDate time.Time) (float64, error) {
-	if len(coupons) == 0 {
-		return -1, errors.New("Failed to calculate the yield due to missing couponsa~Z``````````````````")
-	}
-
-	if !(coupons[0].CouponType == CouponType_COUPON_TYPE_FIX || coupons[0].CouponType == CouponType_COUPON_TYPE_CONSTANT) {
-		return -1, errors.New("Unable to calculate the YTM for non-fixed and non-constant coupons")
-	}
-	holdingPeriod := redemptionDate.Sub(acquisitionDate).Hours() / 24
-
-	yield := (b.NominalValue - marketPrice + totalCouponIncome(coupons, false)) / marketPrice * 365 / holdingPeriod * 100
-	return yield, nil
 }
