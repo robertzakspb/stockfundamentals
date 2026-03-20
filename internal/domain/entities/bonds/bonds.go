66fb12d585baa4a85ec06d6d4bb788edd0d32c5a
Robert Zakiev <zakievrobert78@gmail.com>
2026-03-20 16:48:35 +0100

    Bug fix

--- internal/domain/entities/bonds/bonds.go
+++ internal/domain/entities/bonds/bonds.go
@@ -163,7 +163,7 @@ func (b Bond) YieldToCallOption(coupons []Coupon, marketPrice float64) (float64,
 		return -1, errors.New("Attempting to calculate a yield to call option for a bond without a call exercise date")
 	}
 
-	yield, err := calculateYield(b, coupons, marketPrice, time.Now(), b.MaturityDate)
+	yield, err := calculateYield(b, coupons, marketPrice, time.Now(), b.CallOptionExerciseDate)
 	if err != nil {
 		return -1, err
 	}
@@ -173,6 +173,8 @@ func (b Bond) YieldToCallOption(coupons []Coupon, marketPrice float64) (float64,
 
 // Calculates the return realized on the bond given a market price, including coupons and redemption
 // Coupon reinvestment is not assumed
+//
+//lint:ignore U1000 Ignore unused function temporarily for debugging
 func totalBondReturn(bond Bond, coupons []Coupon, marketPrice float64, acquisitionDate, redemptionDate time.Time) (float64, error) {
 	if marketPrice == 0 {
 		return -1, errors.New("Invalid market price")
@@ -180,7 +182,7 @@ func totalBondReturn(bond Bond, coupons []Coupon, marketPrice float64, acquisiti
 
 	futureCashflows := totalCouponIncome(coupons, false) + bond.NominalValue
 
-	cumulativeReturn := futureCashflows / marketPrice - 1
+	cumulativeReturn := futureCashflows/marketPrice - 1
 	totalReturn := compoundinterest.CalcAnnualizedReturn(cumulativeReturn, acquisitionDate, redemptionDate)
 
 	return totalReturn, nil
