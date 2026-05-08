6da3749e0be5eb488a2b5e06d7b121e80358e0ed
robertzakspb <zakiev@me.com>
2026-05-08 12:31:31 +0200

    Renamed the type

--- internal/domain/entities/fundamentals/financials/models.go
+++ internal/domain/entities/fundamentals/financials/models.go
@@ -1,31 +1,31 @@
-package entity
+package financials
 
 import "github.com/google/uuid"
 
 type FinancialMetric struct {
-	Id       uuid.UUID
-	StockId  uuid.UUID
-	Name     string
-	Period   ReportingPeriod
-	Year     int
-	Value    int
-	Currency string
+	Id              uuid.UUID
+	StockId         uuid.UUID
+	Name            string
+	ReportingPeriod Period
+	Year            int
+	Value           int
+	Currency        string
 }
 
-type ReportingPeriod string
+type Period string
 
 const (
-	Q1            ReportingPeriod = "Q1"
-	Q2            ReportingPeriod = "Q2"
-	Q3            ReportingPeriod = "Q3"
-	Q4            ReportingPeriod = "Q4"
-	CALENDAR_YEAR ReportingPeriod = "CALENDAR_YEAR"
-	H1            ReportingPeriod = "H1"
-	H2            ReportingPeriod = "H2"
-	NINE_MONTHS   ReportingPeriod = "NINE_MONTHS"
+	Q1            Period = "Q1"
+	Q2            Period = "Q2"
+	Q3            Period = "Q3"
+	Q4            Period = "Q4"
+	CALENDAR_YEAR Period = "CALENDAR_YEAR"
+	H1            Period = "H1"
+	H2            Period = "H2"
+	NINE_MONTHS   Period = "NINE_MONTHS"
 )
 
-var ReportingPeriodMap = map[string]ReportingPeriod{
+var ReportingPeriodMap = map[string]Period{
 	"Q1":            Q1,
 	"Q2":            Q2,
 	"Q3":            Q3,
