79ed52022b9258082d183683d1acb166b562a8de
robertzakspb <zakiev@me.com>
2026-05-11 11:55:37 +0200

    Added a couple of response structs to ensure naked strings are not returned in API

--- internal/interface/shared/responseError.go
+++ internal/interface/shared/responseError.go
@@ -1,5 +1,14 @@
 package shared
 
 type ResponseError struct {
-	Errors []string `json:"errors"`
+	Errors  []string `json:"errors"`
+	Message string   `json:"message"`
+}
+
+type StringResponse struct {
+	Message string `json:"message"`
+}
+
+type NumberResponse struct {
+	Value any `json:"value"`
 }
