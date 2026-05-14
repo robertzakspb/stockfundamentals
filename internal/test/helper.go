f572f88ccfc4044fc82dce945c24333b0386822b
robertzakspb <zakiev@me.com>
2026-05-14 14:22:21 +0200

    Enhanced formatting for easier examination

--- internal/test/helper.go
+++ internal/test/helper.go
@@ -16,21 +16,21 @@ func AssertEqualStrings(t testing.TB, expected, actual string) {
 func AssertEqual[T comparable](t testing.TB, expected, actual T) {
 	t.Helper()
 	if expected != actual {
-		t.Errorf("expected: %v; got: %v", expected, actual)
+		t.Errorf("expected: %v;\n got: %v", expected, actual)
 	}
 }
 
 func AssertNotEqual[T comparable](t testing.TB, expected, actual T) {
 	t.Helper()
 	if expected == actual {
-		t.Errorf("Values should not be equal. Expected: %v; got: %v", expected, actual)
+		t.Errorf("Values should not be equal. Expected: %v;\n got: %v", expected, actual)
 	}
 }
 
 func AssertEqualFloat(t testing.TB, expected, actual float64, roundingThreshold float64) {
 	t.Helper()
 	if math.Abs(expected-actual) > roundingThreshold {
-		t.Errorf("Expected: %v; got: %v. Rounding error threshold: %v", expected, actual, roundingThreshold)
+		t.Errorf("Expected: %v;\n got: %v. Rounding error threshold: %v", expected, actual, roundingThreshold)
 	}
 }
 
