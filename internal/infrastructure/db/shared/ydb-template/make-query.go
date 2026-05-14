7ce3ded4368527c1d7565635ceedb4ce8ae143f2
robertzakspb <zakiev@me.com>
2026-05-14 14:50:16 +0200

    Added a template method for generation of YQL SELECT queries for entities

---
+++ internal/infrastructure/db/shared/ydb-template/make-query.go
@@ -0,0 +1,42 @@
+package ydbtemplate
+
+import (
+	"errors"
+	"reflect"
+	"slices"
+	"strings"
+
+	ydbfilter "github.com/compoundinvest/stockfundamentals/internal/infrastructure/db/shared/ydb-filter"
+	taghelpers "github.com/compoundinvest/stockfundamentals/internal/utilities/tag-helpers"
+)
+
+func generateGetQuery[T any](filters []ydbfilter.YdbFilter, tableName string) (string, error) {
+	sb := strings.Builder{}
+
+	sb.WriteString(ydbfilter.AddYqlVarDeclarations(filters))
+	sb.WriteString("SELECT ")
+
+	columnNames, err := taghelpers.GetEntityTagValues[T]("sql")
+	if err != nil {
+		return sb.String(), err
+	}
+
+	//Ensuring the provided filters' column names are present in T's sql tag values
+	for i := range filters {
+		if !slices.Contains(columnNames, filters[i].YqlColumnName) {
+			var t T
+			return sb.String(), errors.New("Column " + filters[i].YqlColumnName + " in a YDB filter is not present in the tag values of entity " + reflect.TypeOf(t).Name())
+		}
+	}
+
+	for i := range columnNames {
+		//Adding the column name to the query
+		sb.WriteString(columnNames[i] + ", ")
+	}
+
+	sb.WriteString("FROM ")
+	sb.WriteString(tableName + " ")
+	sb.WriteString(ydbfilter.MakeWhereClause(filters))
+
+	return sb.String(), nil
+}
---
+++ internal/infrastructure/db/shared/ydb-template/make-query_test.go
@@ -0,0 +1,72 @@
+package ydbtemplate
+
+import (
+	"testing"
+
+	ydbfilter "github.com/compoundinvest/stockfundamentals/internal/infrastructure/db/shared/ydb-filter"
+	"github.com/compoundinvest/stockfundamentals/internal/test"
+	"github.com/ydb-platform/ydb-go-sdk/v3/table/types"
+)
+
+func Test_GenerateGetQuery_NoFilters(t *testing.T) {
+	type Foo struct {
+		a string  `sql:"a"`
+		b float64 `sql:"b"`
+		c bool
+	}
+	tableName := "fooStore"
+	expectedQuery := "SELECT a, b, FROM fooStore "
+
+	query, err :=generateGetQuery[Foo]([]ydbfilter.YdbFilter{}, tableName)
+
+	test.AssertNoError(t, err)
+	test.AssertEqual(t, expectedQuery, query)
+}
+
+func Test_GenerateGetQuery_MissingSqlTag(t *testing.T) {
+	type Foo struct {
+		a string  `sql:"a"`
+		b float64 `sql:"b"`
+		c bool
+	}
+
+	tableName := "fooStore"
+	filters := []ydbfilter.YdbFilter{
+		{
+			YqlColumnName:  "nonExistentTag",
+			Condition:      ydbfilter.GreaterThan,
+			ConditionValue: types.TextValue("test"),
+		},
+	}
+
+	_, err := generateGetQuery[Foo](filters, tableName)
+
+	test.AssertError(t, err)
+}
+
+func Test_GenerateGetQuery_TwoFilters(t *testing.T) {
+	type Foo struct {
+		a string  `sql:"a"`
+		b float64 `sql:"b"`
+		c bool
+	}
+	expectedQuery := "DECLARE $a_filter AS Utf8;\nDECLARE $b_filter AS Double;\nSELECT a, b, FROM fooStore  WHERE\n a > $a_filter AND b <= $b_filter"
+	tableName := "fooStore"
+	filters := []ydbfilter.YdbFilter{
+		{
+			YqlColumnName:  "a",
+			Condition:      ydbfilter.GreaterThan,
+			ConditionValue: types.TextValue("test"),
+		},
+		{
+			YqlColumnName:  "b",
+			Condition:      ydbfilter.LessThanOrEqualTo,
+			ConditionValue: types.DoubleValue(6.7),
+		},
+	}
+
+	query, err := generateGetQuery[Foo](filters, tableName)
+
+	test.AssertNoError(t, err)
+	test.AssertEqual(t, expectedQuery, query)
+}
