45b86436c40f056897786b6287cf211493187555
robertzakspb <zakiev@me.com>
2026-05-15 14:01:51 +0200

    Added a new utility

--- internal/utilities/string-helpers/stringhelpers.go
+++ internal/utilities/string-helpers/stringhelpers.go
@@ -17,3 +17,29 @@ func RemoveDuplicatesFrom(slice []string) []string {
 
 	return noDuplicateSlice
 }
+
+// Given a slice of 15 elements and a size of 4, the method would return 3 slices (3 with 4 elements and the last one with 3 elements)
+func SplitInBatchesOf(size int, strings []string) [][]string {
+	if len(strings) <= size {
+		return [][]string{strings}
+	}
+
+	batches := [][]string{}
+
+	filledBatchCount := len(strings) / size //All but the last batch will be filled completely
+	lastBatchSize := len(strings) % size    //The last batch may have a different size
+
+	for i := range filledBatchCount {
+		batch := strings[size*i : size*(i+1)]
+		batches = append(batches, batch)
+	}
+
+	if lastBatchSize == 0 {
+		return batches
+	}
+
+	lastBatch := strings[filledBatchCount*size:]
+	batches = append(batches, lastBatch)
+
+	return batches
+}
--- internal/utilities/string-helpers/stringhelpers_test.go
+++ internal/utilities/string-helpers/stringhelpers_test.go
@@ -36,3 +36,62 @@ func Test_RemoveDuplicatesFrom_TwoDuplicates(t *testing.T) {
 	test.AssertEqual(t, "dates", noDuplicates[3])
 	test.AssertEqual(t, "orange", noDuplicates[4])
 }
+
+func Test_SplitInBatchesOf_NoElements(t *testing.T) {
+	emptySlice := []string{}
+
+	batches := SplitInBatchesOf(4, emptySlice)
+
+	test.AssertEqual(t, 1, len(batches))
+	test.AssertEqual(t, 0, len(batches[0]))
+}
+
+func Test_SplitInBatchesOf_SizeExceedsSliceLength(t *testing.T) {
+	slice := []string{"test1", "test2"}
+
+	batches := SplitInBatchesOf(4, slice)
+
+	test.AssertEqual(t, "test1", batches[0][0])
+	test.AssertEqual(t, "test2", batches[0][1])
+	test.AssertEqual(t, 1, len(batches))
+}
+
+func Test_SplitInBatchesOf_OneBatch(t *testing.T) {
+	emptySlice := []string{"test1", "test2"}
+
+	batches := SplitInBatchesOf(2, emptySlice)
+
+	test.AssertEqual(t, 1, len(batches))
+	test.AssertEqual(t, "test1", batches[0][0])
+	test.AssertEqual(t, "test2", batches[0][1])
+}
+
+func Test_SplitInBatchesOf_MultipleEquallySizedBatches(t *testing.T) {
+	slice := []string{"test1", "test2", "test3", "test4", "test5", "test6"}
+
+	batches := SplitInBatchesOf(3, slice)
+
+	test.AssertEqual(t, 2, len(batches))
+	test.AssertEqual(t, "test1", batches[0][0])
+	test.AssertEqual(t, "test2", batches[0][1])
+	test.AssertEqual(t, "test3", batches[0][2])
+	test.AssertEqual(t, "test4", batches[1][0])
+	test.AssertEqual(t, "test5", batches[1][1])
+	test.AssertEqual(t, "test6", batches[1][2])
+}
+
+func Test_SplitInBatchesOf_MultipleBatchesWithLastUnfilledBatch(t *testing.T) {
+	slice := []string{"test1", "test2", "test3", "test4", "test5", "test6", "test7", "test8"}
+
+	batches := SplitInBatchesOf(3, slice)
+
+	test.AssertEqual(t, 3, len(batches))
+	test.AssertEqual(t, "test1", batches[0][0])
+	test.AssertEqual(t, "test2", batches[0][1])
+	test.AssertEqual(t, "test3", batches[0][2])
+	test.AssertEqual(t, "test4", batches[1][0])
+	test.AssertEqual(t, "test5", batches[1][1])
+	test.AssertEqual(t, "test6", batches[1][2])
+	test.AssertEqual(t, "test7", batches[2][0])
+	test.AssertEqual(t, "test8", batches[2][1])
+}
