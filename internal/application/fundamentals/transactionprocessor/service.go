58cd96367ee73eedbb887367b1fa626ed8526721
Robert Zakiev <zakievrobert78@gmail.com>
2026-05-28 17:48:04 +0200

    Continued work on transactions

--- internal/application/fundamentals/transactionprocessor/service.go
+++ internal/application/fundamentals/transactionprocessor/service.go
@@ -2,8 +2,13 @@ package transactionprocessor
 
 import (
 	"errors"
+	"sort"
 
 	accountservice "github.com/compoundinvest/stockfundamentals/internal/application/account/account"
+	bondportfolio "github.com/compoundinvest/stockfundamentals/internal/application/account/bond-portfolio"
+	"github.com/compoundinvest/stockfundamentals/internal/domain/entities/account/account"
+	"github.com/compoundinvest/stockfundamentals/internal/domain/entities/bonds"
+	"github.com/compoundinvest/stockfundamentals/internal/domain/entities/portfolio/lot"
 	"github.com/compoundinvest/stockfundamentals/internal/domain/entities/transaction"
 )
 
@@ -17,15 +22,26 @@ func ProcessOrderExecutions(transactions []transaction.Transaction) error {
 		}
 	}
 
-	accountIds := ExtractAccountsFrom(transactions)
-	_, err := accountservice.GetAccountsById(accountIds)
+	accounts, err := accountservice.GetAccountsById(ExtractAccountsFrom(transactions))
 	if err != nil {
 		return err
+	}
+	groupedTransactions := GroupByAccount(transactions)
+
+	for accountId, accountTransactions := range groupedTransactions {
+		account, err := accountservice.FindAccountById(accountId, accounts)
+		if err != nil {
+			return errors.New("Failed to find account " + accountId.String() + " in the list, abandoning the order execution processing")
+		}
 	}
 
 	return nil
 }
 
-func AdjustPositionLots() {
+func AdjustStockLotsAndCashBalances(transactions map[account.Account][]transaction.Transaction, lots []lot.Lot) {
+	sort.Slice()
 
+	//TODO: Adjust the account's cash balance
 }
+
+func AdjustBondLotsAndCashBalances(transactions map[account.Account][]transaction.Transaction, lots []bonds.BondLot)
