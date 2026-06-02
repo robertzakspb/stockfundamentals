e27769bb1ec7d2cc6018a231d98eaa926cc6cc3d
robertzakspb <zakiev@me.com>
2026-06-02 15:17:15 +0200

    Cash balance and lot adjustment logic + unit tests

--- internal/application/fundamentals/transactionprocessor/calculation.go
+++ internal/application/fundamentals/transactionprocessor/calculation.go
@@ -7,32 +7,73 @@ import (
 	"github.com/compoundinvest/stockfundamentals/internal/domain/entities/account/account"
 	"github.com/compoundinvest/stockfundamentals/internal/domain/entities/portfolio/lot"
 	"github.com/compoundinvest/stockfundamentals/internal/domain/entities/transaction"
+	"github.com/google/uuid"
 )
 
 func recalculateLotsAndCashBalances(account account.Account, transactions []transaction.Transaction, lots []lot.Lot) (account.Account, []lot.Lot, error) {
+	if account.Id == uuid.Nil || len(transactions) == 0 || len(lots) == 0 {
+		return account, lots, errors.New("Invalid date was provided to the recalculate lots and balances function")
+	}
 	//Transactions must be processed in chronological order, reenacting the user's behavior in the OMS
 	sort.Slice(transactions, func(i, j int) bool {
-		return transactions[i].Timestamp.After(transactions[j].Timestamp)
+		return transactions[i].Timestamp.Before(transactions[j].Timestamp)
 	})
 	//Lots must be sorted by creation date because sales are applied according to the FIFO principle
 	sort.Slice(lots, func(i, j int) bool {
-		return lots[i].CreatedAt.After(lots[j].CreatedAt)
+		return lots[i].CreatedAt.Before(lots[j].CreatedAt)
 	})
-	//TODO: Check the sorting logic!!
 
 	for _, t := range transactions {
-		if !(t.IsBuyOrder() || t.IsSellOrder()) {
+		if t.Type != transaction.OrderExecution {
 			return account, lots, errors.New("Encountered an unsupported transaction type: " + string(t.Type))
 		}
+		if !(t.IsBuyOrder() || t.IsSellOrder()) {
+			return account, lots, errors.New("Encountered an unsupported order side: " + string(t.Side))
+		}
 		if t.IsBuyOrder() {
+			//Each buy transaction begets a separate transaction lot
 			newLot, err := lot.NewLot(t.Figi, t.Quantity, t.PricePerUnit, t.Currency, t.AccountId)
 			if err != nil {
 				return account, lots, err
 			}
 			lots = append(lots, newLot)
+
+			account.CashBalance -= t.Quantity * t.PricePerUnit
+			if account.IsCashNegative() {
+				return account, lots, errors.New("The cash balance of account " + account.Id.String() + " is negative as a result of applying transaction" + t.Id.String())
+			}
 		}
 		if t.IsSellOrder() {
-			//TODO:
+			//A sale might impact 1 to N lots; hence we need to loop through all lots and start closing them one by one until the transaction quantity is exhausted
+			quantityToSell := t.Quantity
+			targetLotIndices := lot.FindLotIndicesByFigi(lots, t.Figi)
+			for i := range targetLotIndices {
+				var lot *lot.Lot = &lots[i]
+				if lot.IsClosed {
+					continue
+				}
+				//Once all securities were sold, terminate the loop
+				if quantityToSell == 0 {
+					break
+				}
+
+				//In this simple case the entire transaction only impacts the given lot
+				if quantityToSell < lot.Quantity {
+					lot.Quantity -= quantityToSell
+					account.CashBalance += quantityToSell * t.PricePerUnit
+					quantityToSell = 0
+					break
+				}
+				//In this scenario the given lot is not sufficient to cover the transaction and it's thus closed, and the loop proceeds to cover the next lot
+				quantityToSell -= lot.Quantity                       //Reducing the quantity left to sell by the lot's quantity, as it's being closed
+				account.CashBalance += lot.Quantity * t.PricePerUnit //Increasing the balance by the lot's quantity multiplied by the sale price
+				lot.Quantity = 0                                     // Setting the lot's quantity to 0, as it's being closed
+				lot.IsClosed = true                                  //Closing the lot
+			}
+
+			if quantityToSell != 0 {
+				return account, lots, errors.New("Quantity to sell is not 0 for transaction" + t.Id.String())
+			}
 		}
 
 	}
--- internal/application/fundamentals/transactionprocessor/calculation_test.go
+++ internal/application/fundamentals/transactionprocessor/calculation_test.go
@@ -1,1 +1,201 @@
 package transactionprocessor
+
+import (
+	"testing"
+	"time"
+
+	"github.com/compoundinvest/stockfundamentals/internal/domain/entities/account/account"
+	"github.com/compoundinvest/stockfundamentals/internal/domain/entities/portfolio/lot"
+	"github.com/compoundinvest/stockfundamentals/internal/domain/entities/transaction"
+	"github.com/compoundinvest/stockfundamentals/internal/test"
+	"github.com/google/uuid"
+)
+
+func Test_Recalculate_NilAccountId(t *testing.T) {
+	account := account.Account{}
+	lots := []lot.Lot{{}}
+	transactions := []transaction.Transaction{{}}
+
+	_, _, err := recalculateLotsAndCashBalances(account, transactions, lots)
+
+	test.AssertError(t, err)
+}
+
+func Test_Recalculate_NoTransactions(t *testing.T) {
+	account := account.Account{Id: uuid.New()}
+	lots := []lot.Lot{{}}
+	transactions := []transaction.Transaction{}
+
+	_, _, err := recalculateLotsAndCashBalances(account, transactions, lots)
+
+	test.AssertError(t, err)
+}
+
+func Test_Recalculate_NoLots(t *testing.T) {
+	account := account.Account{Id: uuid.New()}
+	lots := []lot.Lot{}
+	transactions := []transaction.Transaction{{}}
+
+	_, _, err := recalculateLotsAndCashBalances(account, transactions, lots)
+
+	test.AssertError(t, err)
+}
+
+func Test_Recalculate_SingleBuy_NegativeCashBalance(t *testing.T) {
+	account := account.Account{Id: uuid.New(), CashBalance: 50}
+	lots := []lot.Lot{{Figi: "figi1", Quantity: 5}}
+	transactions := []transaction.Transaction{
+		{Figi: "figi2", Timestamp: time.Now()},
+		{Figi: "figi1", Timestamp: time.Now().AddDate(0, 0, -7), Quantity: 10, PricePerUnit: 25},
+	}
+
+	_, _, err := recalculateLotsAndCashBalances(account, transactions, lots)
+
+	test.AssertError(t, err)
+}
+
+func Test_Recalculate_SingleBuy_UnsupportedTransactionType(t *testing.T) {
+	account := account.Account{Id: uuid.New(), CashBalance: 400}
+	lots := []lot.Lot{{Figi: "figi1", Quantity: 5}}
+	transactions := []transaction.Transaction{
+		{Figi: "figi2", Timestamp: time.Now()},
+		{Figi: "figi1", Timestamp: time.Now().AddDate(0, 0, -7), Quantity: 10, PricePerUnit: 25},
+	}
+
+	account, lots, err := recalculateLotsAndCashBalances(account, transactions, lots)
+
+	test.AssertError(t, err)
+}
+
+func Test_Recalculate_SingleBuy_PositiveCase(t *testing.T) {
+	account := account.Account{Id: uuid.New(), CashBalance: 400}
+	lots := []lot.Lot{{Figi: "figi1", Quantity: 5, IsClosed: false}}
+	transactions := []transaction.Transaction{
+		{Figi: "figi1", Timestamp: time.Now().AddDate(0, 0, -7), Quantity: 10, PricePerUnit: 25, Type: transaction.OrderExecution, Side: transaction.Buy, Currency: "USD"},
+	}
+
+	account, lots, err := recalculateLotsAndCashBalances(account, transactions, lots)
+
+	test.AssertNoError(t, err)
+	test.AssertEqual(t, 2, len(lots))
+	test.AssertEqual(t, "figi1", lots[0].Figi)
+	test.AssertEqual(t, 5, lots[0].Quantity)
+	test.AssertFalse(t, lots[0].IsClosed)
+	test.AssertEqual(t, "figi1", lots[1].Figi)
+	test.AssertEqual(t, 10, lots[1].Quantity)
+	test.AssertFalse(t, lots[1].IsClosed)
+	test.AssertEqual(t, 150, account.CashBalance)
+}
+
+func Test_Recalculate_MultipleBuys(t *testing.T) {
+	account := account.Account{Id: uuid.New(), CashBalance: 400}
+	lots := []lot.Lot{{Figi: "figi1", Quantity: 5}}
+	transactions := []transaction.Transaction{
+		{Figi: "figi1", Timestamp: time.Now().AddDate(0, 0, -7), Quantity: 10, PricePerUnit: 25, Type: transaction.OrderExecution, Side: transaction.Buy, Currency: "USD"},
+		{Figi: "figi1", Timestamp: time.Now().AddDate(0, 0, -6), Quantity: 2, PricePerUnit: 10, Type: transaction.OrderExecution, Side: transaction.Buy, Currency: "USD"},
+		{Figi: "figi2", Timestamp: time.Now().AddDate(0, 0, -5), Quantity: 1, PricePerUnit: 25, Type: transaction.OrderExecution, Side: transaction.Buy, Currency: "USD"},
+	}
+
+	account, lots, err := recalculateLotsAndCashBalances(account, transactions, lots)
+
+	test.AssertNoError(t, err)
+	test.AssertEqual(t, 4, len(lots))
+	test.AssertEqual(t, "figi1", lots[0].Figi)
+	test.AssertEqual(t, 5, lots[0].Quantity)
+	test.AssertFalse(t, lots[0].IsClosed)
+	test.AssertEqual(t, "figi1", lots[1].Figi)
+	test.AssertEqual(t, 10, lots[1].Quantity)
+	test.AssertFalse(t, lots[1].IsClosed)
+	test.AssertEqual(t, "figi1", lots[2].Figi)
+	test.AssertEqual(t, 2, lots[2].Quantity)
+	test.AssertFalse(t, lots[2].IsClosed)
+	test.AssertEqual(t, "figi2", lots[3].Figi)
+	test.AssertEqual(t, 1, lots[3].Quantity)
+	test.AssertFalse(t, lots[3].IsClosed)
+
+	test.AssertEqual(t, 105, account.CashBalance)
+}
+
+func Test_Recalculate_SingleSell_Negative_ExceededAvailableQuantity(t *testing.T) {
+	account := account.Account{Id: uuid.New(), CashBalance: 400}
+	lots := []lot.Lot{{Figi: "figi1", Quantity: 5}}
+	transactions := []transaction.Transaction{
+		{Figi: "figi1", Timestamp: time.Now().AddDate(0, 0, -7), Quantity: 10, PricePerUnit: 25, Type: transaction.OrderExecution, Side: transaction.Sell, Currency: "USD"},
+	}
+
+	account, lots, err := recalculateLotsAndCashBalances(account, transactions, lots)
+
+	test.AssertError(t, err)
+}
+
+func Test_Recalculate_SingleSell_OneLotIsClosed(t *testing.T) {
+	account := account.Account{Id: uuid.New(), CashBalance: 400}
+	lots := []lot.Lot{
+		{Figi: "figi1", Quantity: 5, IsClosed: false},
+		{Figi: "figi1", Quantity: 10, IsClosed: false},
+	}
+	transactions := []transaction.Transaction{
+		{Figi: "figi1", Timestamp: time.Now().AddDate(0, 0, -7), Quantity: 5, PricePerUnit: 25, Type: transaction.OrderExecution, Side: transaction.Sell, Currency: "USD"},
+	}
+
+	account, lots, err := recalculateLotsAndCashBalances(account, transactions, lots)
+
+	test.AssertNoError(t, err)
+	test.AssertEqual(t, 2, len(lots))
+	test.AssertEqual(t, "figi1", lots[0].Figi)
+	test.AssertEqual(t, 0, lots[0].Quantity)
+	test.AssertTrue(t, lots[0].IsClosed)
+	test.AssertEqual(t, "figi1", lots[1].Figi)
+	test.AssertEqual(t, 10, lots[1].Quantity)
+	test.AssertFalse(t, lots[1].IsClosed)
+
+	test.AssertEqual(t, 525, account.CashBalance)
+}
+
+func Test_Recalculate_SingleSell_MultipleLotsAreClosed(t *testing.T) {
+	account := account.Account{Id: uuid.New(), CashBalance: 400}
+	lots := []lot.Lot{
+		{Figi: "figi1", Quantity: 5, IsClosed: false},
+		{Figi: "figi1", Quantity: 10, IsClosed: false},
+	}
+	transactions := []transaction.Transaction{
+		{Figi: "figi1", Timestamp: time.Now().AddDate(0, 0, -7), Quantity: 5, PricePerUnit: 25, Type: transaction.OrderExecution, Side: transaction.Sell, Currency: "USD"},
+		{Figi: "figi1", Timestamp: time.Now().AddDate(0, 0, -7), Quantity: 10, PricePerUnit: 25, Type: transaction.OrderExecution, Side: transaction.Sell, Currency: "USD"},
+	}
+
+	account, lots, err := recalculateLotsAndCashBalances(account, transactions, lots)
+
+	test.AssertNoError(t, err)
+	test.AssertEqual(t, 2, len(lots))
+	test.AssertEqual(t, "figi1", lots[0].Figi)
+	test.AssertEqual(t, 0, lots[0].Quantity)
+	test.AssertTrue(t, lots[0].IsClosed)
+	test.AssertEqual(t, "figi1", lots[1].Figi)
+	test.AssertEqual(t, 0, lots[1].Quantity)
+	test.AssertTrue(t, lots[1].IsClosed)
+	test.AssertEqual(t, 775, account.CashBalance)
+}
+
+func Test_Recalculate_SingleSell_OneFullCloseAndOnePartialClose(t *testing.T) {
+	account := account.Account{Id: uuid.New(), CashBalance: 400}
+	lots := []lot.Lot{
+		{Figi: "figi1", Quantity: 5, IsClosed: false},
+		{Figi: "figi1", Quantity: 10, IsClosed: false},
+	}
+	transactions := []transaction.Transaction{
+		{Figi: "figi1", Timestamp: time.Now().AddDate(0, 0, -7), Quantity: 5, PricePerUnit: 25, Type: transaction.OrderExecution, Side: transaction.Sell, Currency: "USD"},
+		{Figi: "figi1", Timestamp: time.Now().AddDate(0, 0, -7), Quantity: 3, PricePerUnit: 25, Type: transaction.OrderExecution, Side: transaction.Sell, Currency: "USD"},
+	}
+
+	account, lots, err := recalculateLotsAndCashBalances(account, transactions, lots)
+
+	test.AssertNoError(t, err)
+	test.AssertEqual(t, 2, len(lots))
+	test.AssertEqual(t, "figi1", lots[0].Figi)
+	test.AssertEqual(t, 0, lots[0].Quantity)
+	test.AssertTrue(t, lots[0].IsClosed)
+	test.AssertEqual(t, "figi1", lots[1].Figi)
+	test.AssertEqual(t, 7, lots[1].Quantity)
+	test.AssertFalse(t, lots[1].IsClosed)
+	test.AssertEqual(t, 600, account.CashBalance)
+}
