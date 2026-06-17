b302cde8c621f688a46fd4713c15ac9b094e5c91
zakiev <zakiev@me.com>
2026-06-17 17:41:10 +0200

    Adjusted the DTO to expect the number in the string format

--- internal/interface/api/fundamentals/dividend/dto.go
+++ internal/interface/api/fundamentals/dividend/dto.go
@@ -21,7 +21,7 @@ type DividendDTO struct {
 type DividendForecastDTO struct {
 	Figi               string    `json:"figi"`
 	Ticker             string    `json:"ticker"`
-	ExpectedDPS        float64   `json:"expectedDPS"`
+	ExpectedDPS        float64   `json:"expectedDPS,string"`
 	Currency           string    `json:"currency"`
 	PaymentPeriod      string    `json:"paymentPeriod"`
 	Author             string    `json:"forecastAuthor"`
