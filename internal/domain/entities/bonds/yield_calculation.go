d9599b66d69f82f51e7e1361f8a6194239580974
zakiev <zakiev@me.com>
2026-06-21 16:35:39 +0200

    Added a yield to maturity (non-simple) formula

--- internal/domain/entities/bonds/yield_calculation.go
+++ internal/domain/entities/bonds/yield_calculation.go
@@ -6,7 +6,24 @@ import (
 )
 
 func (b Bond) CalcYieldToMaturity(coupons []Coupon, marketPrice float64) (float64, error) {
+	yield, err := calculateYTM(b, coupons, marketPrice, time.Now(), b.MaturityDate)
+	if err != nil {
+		return -1, err
+	}
 
+	return yield, nil
+}
+
+func (b Bond) CalcYieldToCallOption(coupons []Coupon, marketPrice float64) (float64, error) {
+	if b.CallOptionExerciseDate.IsZero() {
+		return -1, errors.New("Attempting to calculate a yield to call option for a bond without a call exercise date")
+	}
+	yield, err := calculateYTM(b, coupons, marketPrice, time.Now(), b.CallOptionExerciseDate)
+	if err != nil {
+		return -1, err
+	}
+
+	return yield, nil
 }
 
 func (b Bond) CalcSimpleYieldToMaturity(coupons []Coupon, marketPrice float64) (float64, error) {
@@ -39,11 +56,10 @@ func calculateSimpleYield(b Bond, coupons []Coupon, marketPricePercentage float6
 		return -1, errors.New("Unable to calculate the YTM for non-fixed and non-constant coupons")
 	}
 	if marketPricePercentage == 0 {
-		yield := -1
-		return float64(yield), errors.New("Unable to calculate the yield due to a missing market price")
+		return -1, errors.New("Unable to calculate the yield due to a missing market price")
 	}
 
-	holdingPeriod := redemptionDate.Sub(acquisitionDate).Hours() / 24
+	holdingPeriodInDays := redemptionDate.Sub(acquisitionDate).Hours() / 24
 
 	marketPriceInCurrency := marketPricePercentage * b.NominalValue / 100
 
@@ -52,6 +68,26 @@ func calculateSimpleYield(b Bond, coupons []Coupon, marketPricePercentage float6
 	tci := TotalCouponIncome(coupons, false, latestCouponDate)
 
 	//Standard simple formula for the calculation of bond yields (coupon reinvestment is not assumed)
-	yield := (b.NominalValue - marketPrice + tci) / marketPrice * 365 / holdingPeriod * 100
+	yield := (b.NominalValue - marketPrice + tci) / marketPrice * 365 / holdingPeriodInDays * 100
 	return yield, nil
+}
+
+func calculateYTM(b Bond, coupons []Coupon, marketPricePercentage float64, acquisitionDate, redemptionDate time.Time) (float64, error) {
+	if len(coupons) == 0 {
+		return -1, errors.New("Failed to calculate the yield due to missing coupons")
+	}
+	if !(coupons[0].CouponType == CouponType_COUPON_TYPE_FIX || coupons[0].CouponType == CouponType_COUPON_TYPE_CONSTANT) {
+		return -1, errors.New("Unable to calculate the YTM for non-fixed and non-constant coupons")
+	}
+	if marketPricePercentage == 0 {
+		return -1, errors.New("Unable to calculate the yield due to a missing market price")
+	}
+
+	annualCoupon := float64(b.CouponCountPerYear) * b.Coupons[0].PerBondAmount
+	marketPriceInCurrency := marketPricePercentage * b.NominalValue / 100
+	holdingPeriodInYears := redemptionDate.Sub(acquisitionDate).Hours() / 24 / 365
+	numberOfCompoundingPeriods := holdingPeriodInYears * float64(b.CouponCountPerYear)
+
+	ytm := (annualCoupon + (b.NominalValue-marketPriceInCurrency)/numberOfCompoundingPeriods) / ((b.NominalValue + marketPriceInCurrency) / 2)
+	return ytm, nil
 }
