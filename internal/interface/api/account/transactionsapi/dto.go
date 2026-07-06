e5a0d442ed5be5cee2ab9c3fb04067229f5ab9c4
zakiev <zakiev@me.com>
2026-07-06 16:55:13 +0200

    Added a new entity + added json tags

--- internal/interface/api/account/transactionsapi/dto.go
+++ internal/interface/api/account/transactionsapi/dto.go
@@ -3,18 +3,37 @@ package transactionsapi
 import (
 	"time"
 
+	accountsapi "github.com/compoundinvest/stockfundamentals/internal/interface/api/account/accounts"
+	portfolioapi "github.com/compoundinvest/stockfundamentals/internal/interface/api/account/stock-portfolio"
 	"github.com/google/uuid"
 )
 
 type TransactionDto struct {
-	Id           uuid.UUID
-	AccountId    uuid.UUID
-	Figi         string
-	Type         string
-	Timestamp    time.Time
-	Side         string
-	Quantity     float64
-	PricePerUnit float64
-	Currency     string
-	Description  string
+	Id           uuid.UUID `json:"id"`
+	AccountId    uuid.UUID `json:"accountId"`
+	Figi         string    `json:"figi"`
+	Type         string    `json:"type"`
+	Timestamp    time.Time `json:"timestamp"`
+	Side         string    `json:"side"`
+	Quantity     float64   `json:"quantity"`
+	PricePerUnit float64   `json:"pricePerUnit"`
+	Currency     string    `json:"currency"`
+	Description  string    `json:"description"`
+}
+
+type TransactionLotRelationDto struct {
+	Id            string    `json:"id"`
+	TransactionId string    `json:"transactionId"`
+	StockLotId    string    `json:"stockLotId"`
+	BondLotId     string    `json:"bondLotId"`
+	Date          time.Time `json:"date"`
+	Quantity      float64   `json:"quantity"`
+}
+
+// Convenience wrapper for the four encapsulated entities
+type AccountsTransactionsLotsRelationsDto struct {
+	AdjustedAccounts []accountsapi.AccountDto    `json:"adjustedAccounts"`
+	Transactions     []TransactionDto            `json:"transactions"`
+	AdjustedLots     []portfolioapi.LotDto       `json:"adjustedLots"`
+	Relations        []TransactionLotRelationDto `json:"relations"`
 }
