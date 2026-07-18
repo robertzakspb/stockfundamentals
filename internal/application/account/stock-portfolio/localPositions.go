64c2d270663a1f96fbab266982c17409026e10d7
zakiev <zakiev@me.com>
2026-07-18 17:57:33 +0200

    Update

--- internal/application/account/stock-portfolio/localPositions.go
+++ internal/application/account/stock-portfolio/localPositions.go
@@ -105,7 +105,7 @@ func vtbLots() []lot.Lot {
 		Id:           uuid.New(),
 		CreatedAt:    time.Now(),
 		UpdatedAt:    time.Now(),
-		Quantity:     696831,
+		Quantity:     702320,
 		PricePerUnit: 2.0035,
 		Currency:     "RUB",
 		Figi:         "TCS60A1014L8",
