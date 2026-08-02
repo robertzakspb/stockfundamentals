937dac0112e58e55ee4f9026babde370771b0df2
zakiev <zakiev@me.com>
2026-08-02 16:07:00 +0200

    Added the missing mapping to DTOs + sorting by date

--- internal/interface/api/account/transactionsapi/get.go
+++ internal/interface/api/account/transactionsapi/get.go
@@ -2,6 +2,7 @@ package transactionsapi
 
 import (
 	"net/http"
+	"slices"
 
 	"github.com/compoundinvest/stockfundamentals/internal/application/account/transactionprocessor"
 	"github.com/gin-gonic/gin"
@@ -14,5 +15,15 @@ func GetAllTransactions(c *gin.Context) {
 		return
 	}
 
-	c.JSON(http.StatusOK, transactions)
+	dtos := mapTransactionsToDto(transactions)
+
+	slices.SortFunc(dtos, func(t1, t2 TransactionDto) int {
+		if t1.Timestamp.Before(t2.Timestamp) {
+			return 1
+		} else {
+			return -1
+		}
+	})
+
+	c.JSON(http.StatusOK, dtos)
 }
