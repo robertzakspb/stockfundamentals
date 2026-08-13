059300efe9f23dcd450f6de7e1a8bde7b2d49b52
zakiev <zakiev@me.com>
2026-08-13 17:53:45 +0200

    Bug fix

--- internal/application/forexservice/helpers.go
+++ internal/application/forexservice/helpers.go
@@ -85,7 +85,7 @@ func collapseRatesIntoTargetCrossRates(currencyPairs []string, rates []ForexRate
 
 	//Adding the USD/X rates first
 	for i := range rates {
-		pairHasAlreadyBeenAdded := slices.Contains(currencyPairs, strings.Join([]string{string(rates[i].Currency1), string(rates[i].Currency2)}, "/"))
+		_, pairHasAlreadyBeenAdded := FindRate(string(rates[i].Currency1), string(rates[i].Currency2), cleanRates)
 		if rates[i].Currency1 == "USD" && !pairHasAlreadyBeenAdded {
 			cleanRates = append(cleanRates, rates[i])
 		}
