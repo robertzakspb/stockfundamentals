5ff7252677d9cc43ec4c291b3ed47b68a598eade
zakiev <zakiev@me.com>
2026-08-19 18:36:19 +0200

    Added a new endpoint

--- internal/application/account/transactionprocessor/get.go
+++ internal/application/account/transactionprocessor/get.go
@@ -3,10 +3,25 @@ package transactionprocessor
 import (
 	"github.com/compoundinvest/stockfundamentals/internal/domain/entities/account/transaction"
 	"github.com/compoundinvest/stockfundamentals/internal/infrastructure/db/account/transactionsdb"
+	"github.com/compoundinvest/stockfundamentals/internal/interface/shared"
 )
 
 func GetAllTransactions() ([]transaction.Transaction, error) {
 	dbModels, err := transactionsdb.GetAllTransactions()
+	if err != nil {
+		return []transaction.Transaction{}, err
+	}
+
+	mappedTransactions, err := mapTransactionDbModelsToTransactions(dbModels)
+	if err != nil {
+		return []transaction.Transaction{}, err
+	}
+
+	return mappedTransactions, nil
+}
+
+func GetFilteredTransactions(query shared.ParsedApiQuery) ([]transaction.Transaction, error) {
+	dbModels, err := transactionsdb.GetFilteredTransactions(query)
 	if err != nil {
 		return []transaction.Transaction{}, err
 	}
