f94ca88b33065786c2cb1c91b8e1f6d1205b4f35
zakiev <zakiev@me.com>
2026-09-10 18:56:53 +0200

    Added methods for the calculation of bonds' YTMs using the new internal formula

--- internal/domain/entities/bonds/yield_calculation.go
+++ internal/domain/entities/bonds/yield_calculation.go
@@ -3,27 +3,42 @@ package bonds
 import (
 	"errors"
 	"time"
+
+	"github.com/compoundinvest/stockfundamentals/internal/domain/entities/compoundinterest"
 )
 
-func (b Bond) CalcYieldToMaturity(coupons []Coupon, marketPrice float64) (float64, error) {
-	yield, err := calculateYTM(b, coupons, marketPrice, time.Now(), b.MaturityDate)
+// Calculates the bond's yield-to-maturity using the internal IRR formula
+func (b *Bond) CalculateYTM(quote float64) error {
+	cashflows, dates, err := b.FutureCashflowsWithDates(false, quote, time.Now())
 	if err != nil {
-		return -1, err
+		return err
 	}
 
-	return yield, nil
+	irr, err := compoundinterest.InternalRateOfReturn(cashflows, dates)
+	if err != nil {
+		return err
+	}
+
+	b.YieldTomaturity = irr
+
+	return nil
 }
 
-func (b Bond) CalcYieldToCallOption(coupons []Coupon, marketPrice float64) (float64, error) {
-	if b.CallOptionExerciseDate.IsZero() {
-		return -1, errors.New("Attempting to calculate a yield to call option for a bond without a call exercise date")
-	}
-	yield, err := calculateYTM(b, coupons, marketPrice, time.Now(), b.CallOptionExerciseDate)
+// Calculates the bond's yield-to-call-option using the internal IRR formula
+func (b *Bond) CalculateYieldToCallOption(quote float64) error {
+	cashflows, dates, err := b.FutureCashflowsWithDates(true, quote, time.Now())
 	if err != nil {
-		return -1, err
+		return err
 	}
 
-	return yield, nil
+	irr, err := compoundinterest.InternalRateOfReturn(cashflows, dates)
+	if err != nil {
+		return err
+	}
+
+	b.YieldToCallOption = irr
+	
+	return nil
 }
 
 func (b Bond) CalcSimpleYieldToMaturity(coupons []Coupon, marketPrice float64) (float64, error) {
@@ -48,8 +63,6 @@ func (b Bond) CalcSimpleYieldToCallOption(coupons []Coupon, marketPrice float64)
 	return yield, nil
 }
 
-
-
 func calculateSimpleYield(b Bond, coupons []Coupon, marketPricePercentage float64, acquisitionDate, redemptionDate, latestCouponDate time.Time) (float64, error) {
 	if len(coupons) == 0 {
 		return -1, errors.New("Failed to calculate the yield due to missing coupons")
@@ -72,26 +85,4 @@ func calculateSimpleYield(b Bond, coupons []Coupon, marketPricePercentage float6
 	//Standard simple formula for the calculation of bond yields (coupon reinvestment is not assumed)
 	yield := (b.NominalValue - marketPrice + tci) / marketPrice * 365 / holdingPeriodInDays * 100
 	return yield, nil
-}
-
-//TODO: Refactor
-//American formula that assumes semi-annual compounding. Must be modified to account for monthly/quarterly coupons (i.e be universal)
-func calculateYTM(b Bond, coupons []Coupon, marketPricePercentage float64, acquisitionDate, redemptionDate time.Time) (float64, error) {
-	if len(coupons) == 0 {
-		return -1, errors.New("Failed to calculate the yield due to missing coupons")
-	}
-	if !(coupons[0].CouponType == CouponType_COUPON_TYPE_FIX || coupons[0].CouponType == CouponType_COUPON_TYPE_CONSTANT) {
-		return -1, errors.New("Unable to calculate the YTM for non-fixed and non-constant coupons")
-	}
-	if marketPricePercentage == 0 {
-		return -1, errors.New("Unable to calculate the yield due to a missing market price")
-	}
-
-	annualCoupon := float64(b.CouponCountPerYear) * b.Coupons[0].PerBondAmount
-	marketPriceInCurrency := marketPricePercentage * b.NominalValue / 100
-	holdingPeriodInYears := redemptionDate.Sub(acquisitionDate).Hours() / 24 / 365
-	numberOfCompoundingPeriods := holdingPeriodInYears * float64(b.CouponCountPerYear)
-
-	ytm := (annualCoupon + (b.NominalValue-marketPriceInCurrency)/numberOfCompoundingPeriods) / ((b.NominalValue + marketPriceInCurrency) / 2)
-	return ytm, nil
 }
